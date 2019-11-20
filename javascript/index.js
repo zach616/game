a94fcd70c9d085ae25d96c71a02a206858ace4c4
zach616 <57763455+zach616@users.noreply.github.com>
2019-11-20 08:07:24 -0600

    Create index.js

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+    <script>
+      function validateForm() {
+  var x = document.forms["form"]["password"].value;
+  if (x == "qwerty") {
+document.getElementById('demo').innerHTML = "okay"
+";
+
+
+  }
+}
+    </script>
